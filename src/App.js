3c603d4cc03d180ba1cc1e0dcd583f7a25b0aa6b
Neeraj verma <neeraj@vega6.com>
2021-02-20 00:48:24 +0530

    fist time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Inbox from "./component/Inbox";
+import DeletedItems from "./component/DeletedItems";
+import Spam from "./component/Spam";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { setInboxDataFromJson } from "./actions/inbox";
+import { setSpamDataFromJson } from "./actions/spam";
 
 function App() {
+  useEffect(() => {
+    store.dispatch(setInboxDataFromJson());
+    store.dispatch(setSpamDataFromJson());
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path='/' component={Inbox} />
+            <Route exact path='/spam' component={Spam} />
+            <Route exact path='/deleted' component={DeletedItems} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
